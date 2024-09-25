80f365dca4c7840b2f94bc458c9fd12ac98c62f5
Neal Gompa <ngompa@fedoraproject.org>
2024-09-25 10:18:32 -0400

    Revert "Add AT-SPI Xwayland property script"
    This is now correctly handled by KWin directly by loading
    scripts from /etc/xdg/Xwayland-session.d.
    
    This reverts commit 920a84a80e7999cefef01b1d0b03b3d3ee7e6598.

--- etc/xdg/plasma-workspace/env/at-spi-xwayland.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Set the properties if and only if wayland with xwayland is present
-if [ -n "$DISPLAY" ] && [ -n "$WAYLAND_DISPLAY" ]; then
-	xprop -root -format AT_SPI_BUS 8s -set 'AT_SPI_BUS' "$( busctl call --user org.a11y.Bus /org/a11y/bus org.a11y.Bus GetAddress | sed 's/s "\(.*\)"/\1/' )"
-fi
