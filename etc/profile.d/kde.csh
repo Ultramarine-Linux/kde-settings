b1c0d10195562fd850f09768659b01a47389b12e
Flos Lonicerae <lonicerae@gmail.com>
2024-10-20 14:06:00 +0800

    Fixed issue when running script with shebang '#!tcsh -e'.
      * Also sync the logic of csh and sh.

--- etc/profile.d/kde.csh
+++ etc/profile.d/kde.csh
@@ -4,7 +4,11 @@ if ( ! $?KDEDIRS ) setenv KDEDIRS /usr
 
 ## When/if using prelinking, avoids use of kdeinit
 if ( ! $?KDE_IS_PRELINKED ) then
-  grep -qs -qs '^PRELINKING=yes' /etc/sysconfig/prelink && \
-  setenv KDE_IS_PRELINKED 1
+  if ( -f /etc/sysconfig/prelink ) then
+    set PRELINKING = `grep -s "^PRELINKING=" /etc/sysconfig/prelink | cut -d"=" -f2`
+    if ( "$PRELINKING" == "yes" )  then
+      setenv KDE_IS_PRELINKED 1
+    endif
+    unset PRELINKING
+  endif
 endif
-
--- etc/profile.d/kde.sh
+++ etc/profile.d/kde.sh
@@ -3,8 +3,12 @@
 [ -z "$KDEDIRS" ] && KDEDIRS="/usr" && export KDEDIRS
 
 ## When/if using prelinking, avoids (some) use of kdeinit
-if [ -z "$KDE_IS_PRELINKED" ] ; then
-  grep -qs '^PRELINKING=yes' /etc/sysconfig/prelink && \
-  KDE_IS_PRELINKED=1 && export KDE_IS_PRELINKED
+if [ -z "$KDE_IS_PRELINKED" ]; then
+  if [ -f /etc/sysconfig/prelink ]; then
+    PRELINKING=`grep -s "^PRELINKING=" /etc/sysconfig/prelink | cut -d"=" -f2`
+    if [ "$PRELINKING" = "yes" ]; then
+      KDE_IS_PRELINKED=1 && export KDE_IS_PRELINKED
+    fi
+    unset PRELINKING
+  fi
 fi
-
